f42eba64450e656432d977752f99061effa71555
Eva1ent <let@ik.me>
2023-10-17 16:57:45 +0800

    refactor(@globals/types): avoid re-creating valibot schemas

--- packages/types/src/global.ts
+++ packages/types/src/global.ts
@@ -7,21 +7,22 @@ export type GlobalDefinition = Record<string, GlobalType>;
 
 export type GlobalPropertyDescriptors = Record<string, PropertyDescriptor>;
 
+export const GlobalDefinitionSchema = v.record(v.string(), v.union([v.literal("readonly"), v.literal("writable")]));
+
 export function isGlobalDefinition(value: unknown): value is GlobalDefinition {
-    return v.is(v.record(v.string(), v.union([v.literal("readonly"), v.literal("writable")])), value);
+    return v.is(GlobalDefinitionSchema, value);
 }
 
+export const GlobalPropertyDescriptorsSchema = v.record(
+    v.string(),
+    v.object({
+        configurable: v.optional(v.boolean()),
+        enumerable: v.optional(v.boolean()),
+        value: v.optional(v.unknown()),
+        writable: v.optional(v.boolean()),
+    }),
+);
+
 export function isGlobalPropertyDescriptors(value: unknown): value is GlobalPropertyDescriptors {
-    return v.is(
-        v.record(
-            v.string(),
-            v.object({
-                configurable: v.optional(v.boolean()),
-                enumerable: v.optional(v.boolean()),
-                value: v.optional(v.unknown()),
-                writable: v.optional(v.boolean()),
-            }),
-        ),
-        value,
-    );
+    return v.is(GlobalPropertyDescriptorsSchema, value);
 }
